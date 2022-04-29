1dfca9ab8b8461c9d13abf825cd56819e244489d
Salma Esmail <90252790+SalmaEsmail@users.noreply.github.com>
2022-04-29 17:28:43 +0200

    Create second.js

---
+++ - Task-7 - JavaScript Part5/DOM 1/02/second.js
@@ -0,0 +1,6 @@
+let Myphoto =document.getElementsByTagName("img")
+for (i=0;i<Myphoto.length;i++){
+    Myphoto[i].src="https://elzero.org/wp-content/themes/elzero/imgs/logo.png"
+    Myphoto[i].alt="Elzero Logo"
+}
+console.log(Myphoto)
